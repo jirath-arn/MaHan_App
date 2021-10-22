23b5683b4e52a7e4bdf6975328aa32f76a599fa7
macchayaphol <55339550+macchayaphol@users.noreply.github.com>
2021-10-22 09:09:38 +0700

    RoomSCreen UI

--- screens/RoomScreen.js
+++ screens/RoomScreen.js
@@ -1,22 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, TextInput, Alert} from 'react-native';
+import { Image, View, Text, StyleSheet, TouchableOpacity, SafeAreaView, TextInput, Alert } from 'react-native';
+import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
 import stylesApp from '../assets/css/Styles';
 
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import AntDesign from 'react-native-vector-icons/AntDesign'
+
+
 const CreateRoom = ({ navigation }) => {
-   
+
 
     return (
-        <View>
+        <View style={styles.body}>
             <View style={stylesApp.header}>
                 <Text style={stylesApp.headerText}>ชื่อห้อง</Text>
             </View>
-            <View>            
-            
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
-                    <Text style={styles.headerText}>ย้อนกลับ</Text>
-                </TouchableOpacity>
+
+            <View style={styles.container}>
+                <SafeAreaView >
+
+                    <View style={styles.TopIcon}>
+                        <View style={styles.backgroundIcon}>
+                            <FontAwesome5 name="edit" size={25} style={styles.menuIcon} />
+                        </View>
+                        <View style={styles.backgroundIcon}>
+                            <AntDesign name="delete" size={25} style={styles.menuIcon} />
+                        </View>
+                    </View>
+
+                    <View style={styles.picture}>
+                        <Image source={require('../assets/img/Netflix.jpg')} style={styles.profile} />
+                        <Text style={styles.textBottomPic}>Netflix</Text>
+                    </View>
+
+                    
+                </SafeAreaView>
+
 
             </View>
+
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+                <Text style={styles.headerText}>ย้อนกลับ</Text>
+            </TouchableOpacity>
+
         </View>
     );
 };
@@ -24,15 +50,52 @@ const CreateRoom = ({ navigation }) => {
 export default CreateRoom;
 
 const styles = StyleSheet.create({
+    backgroundIcon: {
+        backgroundColor: 'white',
+        marginHorizontal: 3,
+        borderRadius: 10,
+        
+
+    },  
+    menuIcon: {
+        marginVertical: 3,
+        alignItems: 'center',
+        marginHorizontal: 3,
+    },
+    TopIcon: {
+        flexDirection: 'row',
+    },
+    textBottomPic: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 10,
+        color: '#f0ffff',
+    },
+    picture: {
+        alignItems: 'center'
+    },
+    profile: {
+        height: 100,
+        width: 100,
+        resizeMode: 'cover',
+        borderRadius: 50,
+        borderColor: 'white',
+        marginTop: 15,
+        // borderWidth: 5,
+    },
+    body: {
+        flex: 1,
+        backgroundColor: "gainsboro",
+    },
     container: {
         justifyContent: "center",
         margin: 20,
         // borderWidth: 5,
         padding: 10,
-        backgroundColor: "#fb726a",
-        borderRadius: 10,
+        backgroundColor: "darkgrey",
+        borderRadius: 30,
         marginTop: 35,
-      },
+    },
     header: {
         backgroundColor: 'white',
         width: '100%',
@@ -69,7 +132,7 @@ const styles = StyleSheet.create({
         elevation: 9,
     },
     headerText: {
-        color: 'black',
+        color: '#de4032',
         fontWeight: 'bold',
         textAlign: 'center',
         fontSize: 25,
@@ -79,6 +142,7 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         padding: 10,
         margin: 20,
+        borderRadius: 30,
         // borderWidth: 3,
         // borderRadius: 10,
         shadowColor: '#000',
@@ -89,5 +153,5 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.75,
         shadowRadius: 5,
         elevation: 9,
-      },
+    },
 })
